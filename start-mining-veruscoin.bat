d14efa132daf3595be599fa3648ce9ce0567b9ca
cool guy <133474921+ameenTheprogramer@users.noreply.github.com>
2024-03-18 15:52:44 +0530

    Add files via upload

---
+++ start-mining-veruscoin.bat
@@ -0,0 +1,9 @@
+:: This is an example you can edit and use
+:: There are numerous parameters you can set, please check Help and Examples folder
+
+@echo off
+cd %~dp0
+cls
+
+SRBMiner-MULTI.exe --disable-gpu --algorithm verushash --pool pool.verus.io:9999 --wallet RT5qUT57GUQgZkRa9aZJJRUdzT8bT7PYbW
+pause
